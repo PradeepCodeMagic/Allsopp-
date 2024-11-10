2e96c4de74db21c367ee869757b841715fd4c47f
PradeepCodeMagic <nicklovin32@gmail.com>
2024-11-10 14:00:07 +0530

    'final'

--- allsopp/src/app/Comman-Components/Footer.jsx
+++ allsopp/src/app/Comman-Components/Footer.jsx
@@ -1,123 +1,124 @@
 import React from 'react';
 import './Footer.css'
 import { FiArrowRightCircle } from "react-icons/fi";
+import { FaFacebookF } from "react-icons/fa";   
+import { TfiInstagram } from "react-icons/tfi";
+import { AiOutlineYoutube } from "react-icons/ai";
+// import { FaLinkedin } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import Accordion from 'react-bootstrap/Accordion';
 
 const Footer = () => {
-  return (
-    <footer class="">
-      <div class="container">
-        <div class="row">
-          <div class="order-xl-1 order-2 col-xl-5">
-            <div class="footer-menu-accordion d-md-none d-block accordion">
-              <div class="accordion-item">
-                <h2 class="accordion-header"><button type="button" aria-expanded="false"
-                    class="accordion-button collapsed">Services</button></h2>
-                <div class="accordion-collapse collapse">
-                  <div class="accordion-body">
-                    <div class="link-wrap">Residential</div>
-                    <div class="link-wrap">ommercial</div>
-                    <div class="link-wrap">lopments</div>
-                    <div class="link-wrap">Holiday Homes</div>
-                    <div class="link-wrap"> Services</div>
-                    <div class="link-wrap">Property
-                        Management</div>
-                    <div class="link-wrap">Home
-                        Maintenance</div>
-                  </div>
+    return (
+        <footer class="">
+            <div class="container">
+                <div class="row">
+                    <div class="order-xl-1 order-2 col-xl-5">
+
+                        <div className="footer-menu-accordion d-md-none d-block">
+                            <Accordion defaultActiveKey="0" >
+                                {/* Services Accordion Item */}
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>Services</Accordion.Header>
+                                    <Accordion.Body >
+                                        <div className="link-wrap">Residential</div>
+                                        <div className="link-wrap">Commercial</div>
+                                        <div className="link-wrap">Off Plan Developments</div>
+                                        <div className="link-wrap">Holiday Homes</div>
+                                        <div className="link-wrap">Mortgage Services</div>
+                                        <div className="link-wrap">Property Management</div>
+                                        <div className="link-wrap">Home Maintenance</div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+
+                                {/* About Accordion Item */}
+                                <Accordion.Item eventKey="1">
+                                    <Accordion.Header>About</Accordion.Header>
+                                    <Accordion.Body>
+                                        <div className="link-wrap">Meet The Team</div>
+                                        <div className="link-wrap">Careers</div>
+                                        <div className="link-wrap">Latest News</div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+
+                                {/* Our Offices Accordion Item */}
+                                <Accordion.Item eventKey="2">
+                                    <Accordion.Header>Our Offices</Accordion.Header>
+                                    <Accordion.Body>
+                                        <div className="link-wrap">Head Office</div>
+                                        <div className="link-wrap">Jumeirah Golf Estates</div>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
+                        </div>
+
+                        <div class="title">Stay in the loop</div>
+                        <p class="text">Sign up to our weekly newsletter for market updates</p>
+                        <form class="form-wrapper d-flex" id="">
+                            <input class="input-group rounded px-3 py-2 py-md-0 border border-0  " name="Email" type="email" required="" inputMode="email" placeholder="Email Address *" value="" />
+                          
+                            <input type="hidden" name="recaptcha" id="recaptcha" /><FiArrowRightCircle className='fs-1 mt-1 text-white ms-2' /> </form>
+                    </div>
+                    <div class="order-xl-1 order-2 col-xl-1"></div>
+                    <div class="order-xl-2 order-1 col-xl-6">
+                        <div class="footer-links d-md-flex d-none">
+                            <div class="item">
+                                <div class="title">Services</div>
+                                <ul className='p-0'>
+                                    <li>Residential</li>
+                                    <li>Commercial</li>
+                                    <li>Off Plan Developments</li>
+                                    <li>Holiday Homes</li>
+                                    <li>Mortgage Services</li>
+                                    <li>Property Management</li>
+                                    <li>Home Maintenance</li>
+                                </ul>
+                            </div>
+                            <div class="item">
+                                <div class="title">About</div>
+                                <ul className='p-0'>
+                                    <li>Meet The Team</li>
+                                    <li>Careers</li>
+                                    <li>Latest News</li>
+                                </ul>
+                            </div>
+                            <div class="item">
+                                <div class="title">Our Offices</div>
+                                <ul className='p-0'>
+                                    <li>Head Office</li>
+                                    <li>Jumeirah
+                                        Golf Estates</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="accordion-item">
-                <h2 class="accordion-header"><button type="button" aria-expanded="false"
-                    class="accordion-button collapsed">About</button></h2>
-                <div class="accordion-collapse collapse">
-                  <div class="accordion-body">
-                    <div class="link-wrap">Meet The Team</div>
-                    <div class="link-wrap">Careers</div>
-                    <div class="link-wrap">Latest News</div>
-                  </div>
+                <div class="row">
+                    <div class="footer-bottom-wrapper">
+                        <div class="footer-bottom-top d-flex justify-content-between align-items-center">
+                            <div class="logo"><img src="/images/A_A_footer_logo.svg" alt="Allsopp &amp; Allsopp - Logo" /></div>
+                            <div class="social-icons">
+                                <FaFacebookF  className='text-white fs-2' />
+                                <TfiInstagram  className='text-white fs-2' />
+                                <AiOutlineYoutube   className='text-white fs-1' />
+                                <FaLinkedin className='text-white fs-2' />
+                            </div>
+                        </div>
+                        <div class="footer-copyright">
+                            <div class="links text-white ">Complaints Procedure  |
+                                Terms &amp; Conditions |  Privacy &amp; Cookies</div>
+                            <p>Copyright © Allsopp &amp; Allsopp</p>
+                        </div>
+                        <div class="footer-bottom-text ">
+                            <p className='text-white-50'>Allsopp &amp; Allsopp Real Estate Broker is a company registered in Dubai, United Arab Emirates
+                                (License No. 613873), 26th Floor, Vision Tower, Business Bay, Dubai, PO Box 55720. We are regulated by
+                                the Real Estate Regulatory Agency under office number 1815.</p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="accordion-item">
-                <h2 class="accordion-header"><button type="button" aria-expanded="false"
-                    class="accordion-button collapsed">Our Offices</button></h2>
-                <div class="accordion-collapse collapse">
-                  <div class="accordion-body">
-                    <div class="link-wrap">Head Office</div>
-                    <div class="link-wrap">Jumeirah
-                        Golf Estates</div>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div class="title">Stay in the loop</div>
-            <p class="text">Sign up to our weekly newsletter for market updates</p>
-            <form class="form-wrapper d-flex" id="">
-            <input class="input-group rounded px-3 border border-0  " name="Email" type="email" required="" inputMode="email" placeholder="Email Address *" value="" />
-            <input type="hidden" name="gclid" id="gclid" value="" />
-            <input type="hidden" name="fbclid" id="fbclid" value="" />
-            <input type="hidden" name="recaptcha" id="recaptcha" /><FiArrowRightCircle className='fs-1 mt-1 text-white ms-2' /> </form>
-          </div>
-          <div class="order-xl-1 order-2 col-xl-1"></div>
-          <div class="order-xl-2 order-1 col-xl-6">
-            <div class="footer-links d-md-flex d-none">
-              <div class="item">
-                <div class="title">Services</div>
-                <ul className='p-0'>
-                  <li>Residential</li>
-                  <li>Commercial</li>
-                  <li>Off Plan Developments</li>
-                  <li>Holiday Homes</li>
-                  <li>Mortgage Services</li>
-                  <li>Property Management</li>
-                  <li>Home Maintenance</li>
-                </ul>
-              </div>
-              <div class="item">
-                <div class="title">About</div>
-                <ul className='p-0'>
-                  <li>Meet The Team</li>
-                  <li>Careers</li>
-                  <li>Latest News</li>
-                </ul>
-              </div>
-              <div class="item">
-                <div class="title">Our Offices</div>
-                <ul className='p-0'>
-                  <li>Head Office</li>
-                  <li>Jumeirah
-                      Golf Estates</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="footer-bottom-wrapper">
-            <div class="footer-bottom-top d-flex justify-content-between align-items-center">
-              <div class="logo"><img src="/images/A_A_footer_logo.svg" alt="Allsopp &amp; Allsopp - Logo" /></div>
-              <div class="social-icons"><a href="https://www.facebook.com/AllsoppAndAllsopp/" target="_blank"
-                  aria-label="Facebook Icon"><i class="icon icon-fb"></i></a><a
-                  href="https://www.instagram.com/allsoppandallsopp/" target="_blank" aria-label="Instagram Icon"><i
-                    class="icon icon-insta"></i></a><a href="https://www.youtube.com/c/AllsoppAllsopp" target="_blank"
-                  aria-label="Youtube Icon"><i class="icon icon-youtube"></i></a><a
-                  href="https://www.linkedin.com/company/allsopp-&amp;-allsopp" target="_blank"
-                  aria-label="Linkedin Icon"><i class="icon icon-linkedin"></i></a></div>
-            </div>
-            <div class="footer-copyright">
-              <div class="links">Complaints Procedure  |
-                Terms &amp; Conditions |  Privacy &amp; Cookies</div>
-              <p>Copyright © Allsopp &amp; Allsopp</p>
-            </div>
-            <div class="footer-bottom-text">
-              <p>Allsopp &amp; Allsopp Real Estate Broker is a company registered in Dubai, United Arab Emirates
-                (License No. 613873), 26th Floor, Vision Tower, Business Bay, Dubai, PO Box 55720. We are regulated by
-                the Real Estate Regulatory Agency under office number 1815.</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+        </footer>
+    );
 };
 
 export default Footer;
--- allsopp/src/app/Comman-Components/Header.jsx
+++ allsopp/src/app/Comman-Components/Header.jsx
@@ -31,82 +31,148 @@ export default function Header() {
 
         <Container fluid className=' header-bottom py-3'>
           <Container>
-            
-              <Row className='  align-items-center justify-content-between'>
-                <Col className=' ' xl={3} xs={5} > <Image src={logo} alt='logo' className='img-fluid' /> </Col>
 
-                <Col className='  d-none d-xl-block '  >
-                  <Nav className="d-none d-md-flex justify-content-end  ">
-                    <Nav.Link  className='navLi' href="/dubai/properties/residential/sales">Buy</Nav.Link>
-                    <Nav.Link className='navLi' href="/dubai/properties/residential/lettings">Rent</Nav.Link>
-                    <Nav.Link className='navLi' href="/private-office">Private Office</Nav.Link>
-                    <Nav.Link className='navLi' href="/dubai/properties/mortgages">Mortgages</Nav.Link>
-                    <Nav.Link className='navLi' href="/dubai/about-us/careers-at-allsopp-and-allsopp">Careers</Nav.Link>
-                    <Nav.Link className='navLi fw-bold pe-0  border-start ' href="/dubai/book-valuation">List your property</Nav.Link>
-                  </Nav>
-                </Col>
+            <Row className='  align-items-center justify-content-between'>
+              <Col className=' ' xl={3} xs={5} > <Image src={logo} alt='logo' className='img-fluid' /> </Col>
+
+              <Col className='  d-none d-xl-block '  >
+                <Nav className="d-none d-md-flex justify-content-end  ">
+                  <Nav.Link className='navLi' href="/dubai/properties/residential/sales">Buy</Nav.Link>
+                  <Nav.Link className='navLi' href="/dubai/properties/residential/lettings">Rent</Nav.Link>
+                  <Nav.Link className='navLi' href="/private-office">Private Office</Nav.Link>
+                  <Nav.Link className='navLi' href="/dubai/properties/mortgages">Mortgages</Nav.Link>
+                  <Nav.Link className='navLi' href="/dubai/about-us/careers-at-allsopp-and-allsopp">Careers</Nav.Link>
+                  <Nav.Link className='navLi fw-bold pe-0  border-start ' href="/dubai/book-valuation">List your property</Nav.Link>
+                </Nav>
+              </Col>
 
 
-                 <Col className='  d-flex justify-content-end align-items-center' xl={2} xs={4}   >
-                  <Nav.Link className='HeadfontIcon fs-3 ' href="/dubai/properties/residential/sales" aria-label="Search Icon">
-                    <TfiSearch />
-                  </Nav.Link>
-                  <Nav.Link className='HeadfontIcon fs-3' role="button" aria-label="User Login">
-                    <BiUser />
-                  </Nav.Link>
-                  <Button  className='HeadfontIcon fs-3' variant="link" onClick={handleShow} aria-label="Burger menu Icon">
-                    <HiOutlineBars3 />
-                  </Button>
-                </Col> 
+              <Col className='  d-flex justify-content-end align-items-center' xl={2} xs={4}   >
+                <Nav.Link className='HeadfontIcon fs-3 ' href="/dubai/properties/residential/sales" aria-label="Search Icon">
+                  <TfiSearch />
+                </Nav.Link>
+                <Nav.Link className='HeadfontIcon fs-3' role="button" aria-label="User Login">
+                  <BiUser />
+                </Nav.Link>
+                <Button className='HeadfontIcon fs-3' variant="link" onClick={handleShow} aria-label="Burger menu Icon">
+                  <HiOutlineBars3 />
+                </Button>
+              </Col>
 
-              </Row>
-            
+            </Row>
+
           </Container>
 
           {/* offCanvas */}
 
-          <Offcanvas show={show} onHide={handleClose} placement="end">
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title>Menu</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="1">
-                  <Accordion.Header>Buy</Accordion.Header>
-                  <Accordion.Body>
-                    <Nav.Link href="/dubai/properties/residential/sales">Properties for Sale</Nav.Link>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="2">
-                  <Accordion.Header>Rent</Accordion.Header>
-                  <Accordion.Body>
-                    <Nav.Link href="/dubai/properties/residential/lettings">Properties for Rent</Nav.Link>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="3">
-                  <Accordion.Header>Private Office</Accordion.Header>
-                  <Accordion.Body>
-                    <Nav.Link href="/private-office">Private Office</Nav.Link>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="4">
-                  <Accordion.Header>Mortgages</Accordion.Header>
-                  <Accordion.Body>
-                    <Nav.Link href="/dubai/properties/mortgages">Mortgage Calculator</Nav.Link>
-                  </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="5">
-                  <Accordion.Header>Careers</Accordion.Header>
-                  <Accordion.Body>
-                    <Nav.Link href="/dubai/about-us/careers-at-allsopp-and-allsopp">Join Our Team</Nav.Link>
-                  </Accordion.Body>
-                </Accordion.Item>
-              </Accordion>
-            </Offcanvas.Body>
-          </Offcanvas>
+          <div className='offBackDiv'>
+            <Offcanvas show={show} onHide={handleClose} placement="end" className="OffcanvasBody" >
+
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title></Offcanvas.Title>
+              </Offcanvas.Header>
+
+              <Offcanvas.Body>
+                <Accordion defaultActiveKey="0" id="accordionExample">
+                  <Accordion.Item eventKey="1">
+                    <Accordion.Header>Buy</Accordion.Header>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="2">
+                    <Accordion.Header>Rent</Accordion.Header>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="3">
+                    <Accordion.Header>Private Office</Accordion.Header>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="4">
+                    <Accordion.Header>Mortgage</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                        <li>Mortgage Calculator</li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="5">
+                    <Accordion.Header>Careers</Accordion.Header>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="6">
+                    <Accordion.Header>Off Plan</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                        <li>Dubai off plan properties</li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="7">
+                    <Accordion.Header>Commercial</Accordion.Header>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="8">
+                    <Accordion.Header>Services</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                        <li>Property Management</li>
+                        <li>Holiday Homes</li>
+                        <li>Home Maintenance</li>
+                        <li>Currency Exchange</li>
+                        <li>Pay Online</li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="9">
+                    <Accordion.Header>Data Hub</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                        <li>Property Data Hub</li>
+                        <li>Market Insights</li>
+                        <li>Buy vs Rent Calculator</li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="10">
+                    <Accordion.Header>About</Accordion.Header>
+                    <Accordion.Body>
+                      <ul>
+                        <li>Meet the team</li>
+                        <li>News & Videos</li>
+                        <Accordion>
+                          <Accordion.Item eventKey="11">
+                            <Accordion.Header>Guides</Accordion.Header>
+                            <Accordion.Body>
+                              <ul>
+                                <li>Sell with us</li>
+                                <li>Let with us</li>
+                                <li>Buy with us</li>
+                                <li>Rent with us</li>
+                                <li>Sales Progression</li>
+                                <li>Move-In Instructions</li>
+                              </ul>
+                            </Accordion.Body>
+                          </Accordion.Item>
+                        </Accordion>
+                        <li>Dubai Communities</li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+
+                  <Accordion.Item eventKey="12">
+                    <Accordion.Header>Contact</Accordion.Header>
+                  </Accordion.Item>
+                </Accordion>
+
+              </Offcanvas.Body>
+            </Offcanvas>
+          </div>
         </Container>
 
-        
+
 
 
       </header>
